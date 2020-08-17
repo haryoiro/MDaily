6d778c3ec966e1cf77e46009de22f9d1b6b9e405
haryoiro <mizusecocolte@gmail.com>
2020-08-17 13:46:07 +0900

    Use Slate.js to crate a simple customized renderable element from initialState.

--- client/src/components/SlateEditor/SlateEditor.jsx
+++ client/src/components/SlateEditor/SlateEditor.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useMemo, useState } from "react";
+import React, { useEffect, useMemo, useState, useCallback } from "react";
 import { createEditor } from 'slate'
 import { Slate, Editable, withReact } from 'slate-react'
 
@@ -9,20 +9,43 @@ export function SlateEditor() {
       type: 'paragraph',
       children: [{ text: 'A line of text in a paragraph.' }],
     },
+    {
+      type: 'code',
+      children: [{ text: 'const a = {text: "text"}'}]
+    }
   ])
 
+  const renderElement = useCallback(props => {
+    switch (props.element.type) {
+      case 'code':
+        return <CodeElement {...props} />
+      default:
+        return <DefaultElement {...props} />
+    }
+  })
+
   return (
-    <Slate
-      editor={editor}
-      value={value}
-      onChange={newValue => setValue(newValue)}
-    >
-      <Editable onKeyDown={e => {
-        if (e.key === '&') {
-          e.preventDefault()
-          editor.insertText('and')
-        }
+    <Slate editor={editor} value={value} onChange={newValue => setValue(newValue)}>
+      <Editable
+        renderElement={renderElement}
+        onKeyDown={e => {
+          if (e.key === '&') {
+            e.preventDefault()
+            editor.insertText('and')
+          }
       }}/>
     </Slate>
   )
+}
+
+function CodeElement({attributes, children}) {
+  return (
+    <pre {...attributes}>
+      <code>{children}</code>
+    </pre>
+  )
+}
+
+function DefaultElement({attributes, children}) {
+return <p {...attributes}>{children}</p>
 }
