0f0364ff25435179074b6eb33610580b1f3dc2a1
haryoiro <mizusecocolte@gmail.com>
2020-08-23 12:09:00 +0900

    Remove unused import and be able to visible List each body

--- client/src/components/Board/BoardList.jsx
+++ client/src/components/Board/BoardList.jsx
@@ -1,21 +1,26 @@
-import React, { useState, useEffect } from 'react';
-import { useDispatch } from 'react-redux'
-import { useQuery, useMutation, queryCache } from 'react-query'
-import { getAll, getDataById } from '../../services/access'
-import { Link, useParams } from 'react-router-dom'
+import React from 'react';
+import { useQuery } from 'react-query'
+import { getAll } from '../../services/access'
+import { Link } from 'react-router-dom'
 
 export function BoardList() {
   const { isLoading, isError, data, error } = useQuery('board', getAll)
 
+  /* TODO */
+  // ローディング, エラー時に表示させる特定のコンポーネントを作成
   if (isLoading) return <div>NOW LOADING...</div>
   if (isError) return <div>{error.message}</div>
-  console.log(data)
 
   return (
     <div>
       {data.map(a =>
         <Link to={`/board/${a.id}`} key={a.id}>
           <h2>{a.title}</h2>
+          <div>
+            {JSON.parse(a.contents.text)
+              .map(b => b.children[0].text)
+              .join('\n')}
+          </div>
         </Link>
       )}
     </div>
