62d6125946ff9c0949b457a04b5a0ca26832f8bd
haryoiro <mizusecocolte@gmail.com>
2020-08-23 12:06:22 +0900

    Redirect new URL when create a board

---
+++ client/src/components/Board/NewBoard.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import { useHistory } from 'react-router-dom'
+import { createNew } from '../../services/access'
+import { useMutation, queryCache } from 'react-query'
+import { useDispatch } from 'react-redux'
+import { asyncNotification } from '../Notification/notificationSlice'
+
+export function NewBoard() {
+  const history = useHistory();
+  const dispatch = useDispatch()
+  const [createNote] = useMutation(createNew, {
+    onSuccess: async (data) => {
+      queryCache.invalidateQueries('board')
+      await dispatch(asyncNotification(`CONTENT IS SAVED`))
+    },
+    onError: async (data, error) => {
+      await dispatch(asyncNotification(error.message))
+    }
+  })
+
+  async function onCreateNew() {
+      const { id } = await createNote()
+      history.push(`/board/${id}`)
+  }
+
+  return (
+    <button onClick={onCreateNew}>
+      <span>➕</span>
+    </button>
+  )
+}
