a7937b31441947d1c5156c07441e8ad5394557f6
haryoiro <mizusecocolte@gmail.com>
2020-08-23 19:53:39 +0900

    Full featured Markdown editor

--- client/src/components/Board/Board.jsx
+++ client/src/components/Board/Board.jsx
@@ -1,72 +1,91 @@
 import React, { useState, useEffect, useCallback, useMemo } from 'react'
-import { Slate, Editable, withReact } from 'slate-react'
-import { createEditor } from 'slate'
-import { withHistory } from 'slate-history'
-import { useQuery, useMutation, queryCache } from 'react-query'
 import { useParams } from 'react-router-dom'
-import { useDispatch } from 'react-redux'
-import { getDataById, updateDataById } from '../../services/access'
+// API Access
+import { updateDataById } from '../../services/access'
+// Slate
+import { createEditor } from 'slate'
+import { Slate, Editable, withReact } from 'slate-react'
+// Slate Plugin
+import { withHistory } from 'slate-history'
 import { withShortcuts } from './withShortcuts'
-import { asyncNotification } from '../Notification/notificationSlice'
+// Elements
 import { MarkedElements } from './MarkedElements'
+import { Leaf } from './Leaf'
 
-export const Board = () => {
+import { useAutoSave } from '../../hooks/useAutoSave'
+
+function generateSlateNode(type, text = '') {
+  return [{ type, children: [{ text }] }]
+}
+
+export const Board = ({ datas, refetch }) => {
   const { id } = useParams()
-  const [setText, setSendTitle, saveText] = useAutoSave(id)
-  const [content, setContent] = useState([{ type: 'paragraph', children: [{ text: '' }] }])
-  const [title, setTitle] = useState([{ type: 'heading-one', children: [{ text: '' }] }])
-  const { isLoading, isError, data, error } = useQuery(['board', { id }], getDataById)
+  const data = datas.find(n => n.id === id)
+  // AutoSaving Hooks
+  const [text, setText, saveText] = useAutoSave(updateDataById, id, 'text', refetch)
+  const [title, setTitle, saveTitle] = useAutoSave(updateDataById, id, 'title', refetch)
+  // Editor Initialize
+  const [currentText, setCurrentText] = useState(generateSlateNode('paragraph'))
+  const [currentTitle, setCurrentTitle] = useState(generateSlateNode('heading-one'))
+
   const renderElement = useCallback(props => <MarkedElements {...props} />, [])
+  const renderLeaf = useCallback(props => <Leaf {...props} />, [])
+  // Settings for a Slate Editor
   const editor = useMemo(() => withShortcuts(withReact(withHistory(createEditor()))), [])
   const titleEditor = useMemo(() => withReact(withHistory(createEditor())), [])
+  const decorate = useCallback(markdownDecorator, [])
 
   useEffect(() => {
     if (data) {
-      const parsedBodyText = JSON.parse(data.contents.text)
-      setContent(parsedBodyText)
-      setTitle([{ type: 'heading-one', children: [{ text: data.title }] }])
-      setSendTitle(data.title)
+      setCurrentText(JSON.parse(data.contents.text))
+      setCurrentTitle(generateSlateNode('heading-one', data.title))
+      setTitle(data.title)
     }
   }, [data])
 
-  if (isLoading) return <div>NOW LOADING...</div>
-  if (isError) return <div>{error.message}</div>
-
   function onTitleChange(value) {
-    setTitle(value)
+    setCurrentTitle(value)
     const titleText = value[0].children[0].text
-    if (titleText === '') { setSendTitle('UNTITLED') }
-    setSendTitle(titleText)
+    setTitle(titleText)
+  }
+
+  function onHotKey(e) {
+    if (e.key === 's' && ((e.ctrlKey && !e.metaKey) || (!e.ctrlKey && e.metaKey))) {
+      e.preventDefault()
+      if (currentText !== text) {
+        console.log('text saved')
+        saveText()
+      }
+      if (currentTitle !== title) {
+        console.log('title saved')
+        saveTitle()
+      }
+    }
   }
 
   return (
     <>
-      <Slate editor={titleEditor} value={title} onChange={onTitleChange}>
-        <Editable renderElement={renderElement} onKeyDown={e => {
-          if (e.key === 'Enter') { e.preventDefault() }
-          if ((e.key === 's') && ((e.ctrlKey && !e.metaKey) || (!e.ctrlKey && e.metaKey))) {
+      <Slate editor={titleEditor} value={currentTitle} onChange={onTitleChange}>
+        <Editable renderElement={renderElement} placeholder="Write some Title" onKeyDown={e => {
+          if (e.key === 's' && ((e.ctrlKey && !e.metaKey) || (!e.ctrlKey && e.metaKey))) {
             e.preventDefault()
-            saveText()
+            if (currentText !== text) saveText()
+            if (currentTitle !== title) saveTitle()
           }
-        }}
-          placeholder="Write Some Title"
-        />
+        }} />
       </Slate>
-      <Slate editor={editor} value={content} onChange={value => {
-        setContent(value)
-        setText(JSON.stringify(value))
+      <Slate editor={editor} value={currentText} onChange={v => {
+        setCurrentText(v)
+        setText(JSON.stringify(v))
       }}>
         <Editable
+          decorate={decorate}
           renderElement={renderElement}
+          renderLeaf={renderLeaf}
           placeholder="Write some markdown..."
           autoFocus
           className="editor"
-          onKeyDown={e => {
-            if (e.key === 's' && ( (e.ctrlKey && !e.metaKey) || (!e.ctrlKey && e.metaKey) )) {
-              e.preventDefault()
-              saveText()
-            }
-          }}
+          onKeyDown={onHotKey}
         />
       </Slate>
     </>
@@ -74,41 +93,3 @@ export const Board = () => {
 
 }
 
-
-function useAutoSave(currentId) {
-  const dispatch = useDispatch()
-  const [lastText, setLastText] = useState('')
-  const [text, setText] = useState('')
-  const [lastTitle, setLastTitle] = useState('')
-  const [title, setTitle] = useState('')
-  const [updateContent] = useMutation(updateDataById, {
-    onSuccess: async (data) => {
-      queryCache.invalidateQueries('board')
-      await dispatch(asyncNotification(`CONTENT IS SAVED`))
-    },
-    onError: async (data, error) => {
-      await dispatch(asyncNotification(error.message))
-    }
-  })
-  useEffect(() => {
-    if ((text !== lastText) || (title !== lastTitle)) {
-      const timer = setTimeout(async () => {
-        await updateContent([currentId, text, title])
-        setLastText(text)
-        setLastTitle(title)
-      }, AUTO_SAVE_INTERVAL)
-      return () => clearTimeout(timer)
-    }
-  }, [text, title, lastText, currentId, lastTitle, updateContent])
-  const AUTO_SAVE_INTERVAL = 10000
-
-  async function saveText() {
-    await updateContent([currentId, text, title])
-  }
-
-  return [
-    setText,
-    setTitle,
-    saveText,
-  ]
-}
