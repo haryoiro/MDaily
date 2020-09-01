ad91ebae5f6fc5ed5e9bf4bc677e903c1b463308
haryoiro <mizusecocolte@gmail.com>
2020-09-01 23:28:50 +0900

    暫定的にStyled-Componentsをファイルごと一箇所にまとめる

--- client/src/components/Board/BoardList.jsx
+++ client/src/components/Board/BoardList.jsx
@@ -1,78 +1,60 @@
-import React, { useState } from 'react';
+import React, { useState } from 'react'
 import styled from 'styled-components'
-import { Grid, Link } from '../shared'
 import { useQuery } from 'react-query'
-import { useDispatch } from 'react-redux'
-import { allBoard } from './boardSlice'
 import { getAll } from '../../services/access'
-import { Slider } from '../shared/Slider'
-import { Header } from '../../components/'
+import { Link, Slider } from '../shared'
+import Header from '../Header/Header'
 
 function BoardList() {
-  const dispatch = useDispatch()
-  const { isLoading, isError, data, error } = useQuery('board', () => getAll())
+  const {
+    isLoading, isError, data, error, refetch,
+  } = useQuery('board', () => getAll())
   const [sliderValue, setSliderValue] = useState(0)
-  
+
   if (isLoading) return <div>NOW LOADING...</div>
   if (isError) return <div>{error.message}</div>
 
-  function parseTextFromSlate(a) {
-    return JSON.parse(a.contents.text)
-      .map(b => b.children[0].text)
-      .join('\n')
+  function parseContentFromSlate(a) {
+    const [title, ...description] = JSON.parse(a.contents.text)
+    return {
+      title: title.children[0].text !== '' ? title.children[0].text : 'Untitled',
+      description: description.map((b) => b.children[0].text).join('\n'),
+    }
   }
-  
   return (
     <>
-      <Header />
-      <BoardLayout index={sliderValue}>
-        {data.map(a =>
-          <ItemWrapper key={a.id}>
-            <BoardItem to={`/board/${a.id}`} key={a.id}>
-              <BoardHeader>{a.title}</BoardHeader>
-              <BoardBody>{parseTextFromSlate(a)}</BoardBody>
-            </BoardItem>
-          </ItemWrapper>
-        )}
-      </BoardLayout>
-      <Slider
-        value={sliderValue}
-        onChange={(e) => setSliderValue(e.target.value)}
-      />
+      <Header refetch={refetch}>
+        <Slider
+          value={sliderValue}
+          onChange={(e) => setSliderValue(e.target.value)}
+        />
+      </Header>
+
+      <CardLayout index={sliderValue} className="card-layout">
+        {data.map((a) => (
+          <div key={a.id} className="card-wrapper">
+            <Link to={`/board/${a.id}`} className="card-item">
+
+              <div className="card-header">
+                {parseContentFromSlate(a).title}
+              </div>
+
+              <div className="card-description">
+                {parseContentFromSlate(a).description}
+              </div>
+
+            </Link>
+          </div>
+        ))}
+      </CardLayout>
     </>
   )
 }
 
-const BoardHeader = styled.div`
-margin: 1rem;
-margin-top: 0.5rem;
-color: ${({ theme }) => theme.fg1};
-font-size: 18px;
-font-weight: medium;
-`
-const BoardBody = styled.div`
-margin: 1rem;
-color: ${({ theme }) => theme.fg2};
-font-size: 14px;
-`
-const BoardItem = styled(Link)`
-text-align: left;
-margin-left: -2px;
-&:hover {
-  background: none;
-}
-`
-const ItemWrapper = styled.div`
-display: flex;
-color: ${({ theme }) => theme.fg1};
-background: ${({ theme }) => theme.bg2};
-border-radius: 5px;
-max-width: 260px;
-`
-const BoardLayout = ({ index, children }) => {
+const CardLayout = ({ index, children, className }) => {
   const layout = [{
     x: '157px',
-    y: '180px',
+    y: '169px',
   }, {
     x: '180px',
     y: '210px',
@@ -82,13 +64,13 @@ const BoardLayout = ({ index, children }) => {
   }]
 
   return (
-    <BoardRow x={layout[index].x} y={layout[index].y}>
+    <CardRow x={layout[index].x} y={layout[index].y} className={className}>
       {children}
-    </BoardRow>
+    </CardRow>
   )
 }
 
-const BoardRow = styled.div`
+const CardRow = styled.div`
 display: grid;
 grid-area: "content";
 grid-column: 2;
@@ -96,9 +78,36 @@ grid-auto-rows: ${({ y }) => y};
 grid-template-columns: repeat(auto-fit, minmax(${({ x }) => x}, 1fr));
 gap: 15px;
 overflow-y: scroll;
-padding: 10px 20px;
-margin: 0px 60px 0px 60px;
+padding: 0px 120px 0 60px;
 height: 100vh;
+.card-wrapper {
+  display: flex;
+  color: ${({ theme }) => theme.fg1};
+  background: ${({ theme }) => theme.bg2};
+  border-radius: 5px;
+  max-width: 260px;
+  .card-item, a{
+    text-align: left;
+    margin-bottom: 1.2rem;
+    width: 100%;
+    overflow: hidden;
+    text-overflow: ellipsis;
+    &:hover {
+      background: none;
+    }
+    .card-header {
+      margin: 0.5rem 0.5rem;
+      color: ${({ theme }) => theme.fg1};
+      font-size: 18px;
+      font-weight: bold;
+    }
+    .card-description {
+      margin: 0.5rem 0.5rem;
+      color: ${({ theme }) => theme.fg2};
+      font-size: 12px;
+    } /* description */
+  } /* item */
+} /* wrapper */
 `
 
 export default BoardList
