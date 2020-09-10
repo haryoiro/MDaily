831339c439d9eb030c0d7d13b06520516d522ca5
haryoiro <mizusecocolte@gmail.com>
2020-09-10 12:34:57 +0900

    レイアウトを修正

--- client/src/components/shared/BodyWrapper.jsx
+++ client/src/components/shared/BodyWrapper.jsx
@@ -4,17 +4,17 @@ const BodyWrapper = styled.div`
   display: grid;
   height: 100vh;
   grid-template-columns: 60px 1fr;
-  grid-template-rows: 128px 1fr;
+  grid-template-rows: 75px 20px 1fr;
   grid-template-areas:
   "sidebar header"
-  "sidebar content"
+  "sidebar toolbar"
   "sidebar content";
   transition: all 0.5s ease-in-out;
   @media screen and (max-width: 550px) {
     grid-template-columns: 0px 1fr;
     grid-template-areas:
       "header"
-      "content"
+      "toolbar"
       "content";
   }
 `
