d3738c2b0c84bfc6c57dc065b4723aa745d5f3c2
haryoiro <mizusecocolte@gmail.com>
2020-09-10 15:42:25 +0900

    ログインするとノートの新規作成ができるよう変更

--- server/routes/note.route.js
+++ server/routes/note.route.js
@@ -1,21 +1,37 @@
 const noteRouter = require('express').Router({ mergeParams: true })
 const { Note } = require('../models')
 const { slateInitialValue } = require('../helpers/initialValues')
+const { authenticateChecker } = require('../middlewares/authenticateChecker')
 const creator = require('../helpers/jsonResponseCreator')
+const { Board } = require('../models/Board.model')
 
+// 新規ノートを作成
 noteRouter.route('/')
-  .post(async (req, res) => {
+  .post(authenticateChecker, async (req, res) => {
+    const { boardName } = req.params
+    const { auth } = req
+
+    const returnedBoard = await Board.findOne({ boardName, ownerId: auth.id }).select('notes')
+    if (!returnedBoard) {
+      return res
+        .status(404)
+        .json(creator(404, 'Board Not Found'))
+    }
+
     const newNote = await new Note({
       ...slateInitialValue,
     })
 
     const savedNote = await newNote.save()
+    // eslint-disable-next-line no-underscore-dangle
+    returnedBoard.notes = [...returnedBoard.notes, savedNote._id]
+    await returnedBoard.save()
 
-    return res.json(savedNote.toJSON())
+    return res.json(savedNote)
   })
 
 noteRouter
-  .route('/id')
+  .route('/:id')
   .get(async (req, res) => {
     const { id } = req.params
 
@@ -28,42 +44,40 @@ noteRouter
 
     return res.json(note.toJSON())
   })
-
-noteRouter.put('/:id', async (req, res) => {
-  const { body } = req
-  const { id } = req.params
-  if (!body) {
-    return res.status(400)
-  }
-  const note = await Note.findOneAndUpdate(
-    { _id: id },
-    {
-      $set: {
-        contents: {
-          text: body.text,
+  .put(async (req, res) => {
+    const { body } = req
+    const { id } = req.params
+    if (!body) {
+      return res.status(400)
+    }
+    const note = await Note.findOneAndUpdate(
+      { _id: id },
+      {
+        $set: {
+          contents: {
+            text: body.text,
+          },
         },
       },
-    },
-  )
-  // mongooseの使用上、save()関数を実行しないと__vが加算されないので
-  // この行は残しておくこと。
-  // issue: https://github.com/Automattic/mongoose/issues/6994
-  // プラグインによっての解決も可能。
-  await note.save()
-  // note: 404ステータス
-  return res.status(204).json(note.toJSON())
-})
+    )
+    // mongooseの使用上、save()関数を実行しないと__vが加算されないので
+    // この行は残しておくこと。
+    // issue: https://github.com/Automattic/mongoose/issues/6994
+    // プラグインによっての解決も可能。
+    await note.save()
+    // note: 404ステータス
+    return res.status(204).json(note.toJSON())
+  })
+  .delete(async (req, res) => {
+    const { id } = req.params
 
-noteRouter.delete('/:id', async (req, res) => {
-  const { id } = req.params
-
-  const message = await Note.findByIdAndRemove(id)
-  if (!message) {
-    return res
-      .status(404)
-      .send(creator(404, `Note id:${id} is not found`))
-  }
-  return res.status(204).json({ success: true, message })
-})
+    const message = await Note.findByIdAndRemove(id)
+    if (!message) {
+      return res
+        .status(404)
+        .send(creator(404, `Note id:${id} is not found`))
+    }
+    return res.status(204).json({ success: true, message })
+  })
 
 module.exports = noteRouter
