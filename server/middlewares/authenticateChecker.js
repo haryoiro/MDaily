89778821ab46de75650176775b49ab6b9969bc70
haryoiro <mizusecocolte@gmail.com>
2020-09-10 12:41:29 +0900

    有効なトークンが含まれているかチェックするミドルウェア

--- server/middlewares/authenticateChecker.js
+++ server/middlewares/authenticateChecker.js
@@ -1,25 +1,36 @@
-const User = require('../models/User')
+const jwt = require('jsonwebtoken')
+const { User } = require('../models/User.model')
+const creator = require('../helpers/jsonResponseCreator')
+const { SECRET } = require('../configs')
 
 async function checkDuplicateUserNameOrEmail(req, res, next) {
   const { username, email } = req.body
+  const message = creator(400, 'Conflict', 'Duplicate Username or Email')
 
-  const errorMessage = {
-    success: false,
-    code: 400,
-    auth: false,
-    token: null,
-    message: 'Duplicate Username or Email ',
+  const isDuplicatedUser = await User.exists({ username })
+  if (isDuplicatedUser) return res.status(409).json(message)
+
+  const isDuplicatedEmail = await User.exists({ email })
+  if (isDuplicatedEmail) return res.status(409).json(message)
+
+  return next()
+}
+
+async function authenticateChecker(req, res, next) {
+  const authHeader = req.headers.authorization
+  const token = authHeader && authHeader.split(' ')[1]
+  console.log(authHeader, token)
+  if (!token) return res.status(401)
+
+  const isAuth = await jwt.verify(token, SECRET)
+  if (!isAuth) {
+    return res.status(403)
   }
-
-  const isDuplicatedUser = await User.findOne({ username })
-  if (isDuplicatedUser) return res.status(400).json(errorMessage)
-
-  const isDuplicatedEmail = await User.findOne({ email })
-  if (isDuplicatedEmail) return res.status(400).json(errorMessage)
-
+  req.auth = isAuth
   return next()
 }
 
 module.exports = {
   checkDuplicateUserNameOrEmail,
+  authenticateChecker,
 }
