b010f4349aec36c54d3aa36b034b8077e39505fe
haryoiro <mizusecocolte@gmail.com>
2020-09-17 10:03:14 +0900

    NavBarの仮コンポーネントを追加

---
+++ client/src/components/organisms/NavBar/NavBar.fixture.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import NavBar from './NavBar'
+import Prov from '../../../theme/provider'
+
+export default () => (
+  <Prov>
+    <NavBar />
+  </Prov>
+)
--- client/src/components/organisms/NavBar/NavBar.jsx
+++ client/src/components/organisms/NavBar/NavBar.jsx
@@ -1,10 +1,42 @@
+/** @jsx jsx */
 import React from 'react'
-function NavBar() {
-  return (
-    <Flex>
+import { Flex, Box,  Link, css, jsx } from 'theme-ui'
 
-    </Flex>
+
+const NavStyle = css({
+  height: '3em',
+  border: "1px solid",
+  alignItems: "center",
+  listStyle: "none",
+  "li:not(:first-of-type) a, li:not(:first-of-type) button": {
+    "margin-left": "1rem"
+  },
+  a: {
+    textDecoration: "none",
+    textTransform: "uppercase",
+    color: "text",
+    fontSize: 1,
+    fontWeight: "bold",
+    letterSpacing: 1
+  },
+  "a:hover": {
+    textDecoration: "underline"
+  }
+})
+
+export default function NavBar(props) {
+  return (
+    <Box >
+      <Flex
+        px={3}
+        alignItems='center'
+        sx={NavStyle} >
+        <li><Link to="/board">Home</Link></li>
+        <Box mx='auto' />
+        <li><Link to="/board">New</Link></li>
+        <Box mx='auto' />
+        <li><Link to="/board">Profile</Link></li>
+      </Flex>
+    </Box>
   )
 }
-
-export default NavBar
